6eec00a393483f11d17cf42f1b246b8af77ffe8d
Andrey Alymov <dupodupovich@gmail.com>
2021-09-06 15:27:59 +0300

    continue

---
+++ LightChat/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  LightChat
+//
+//  Created by Andrey Alymov on 31.08.2021.
+//
+
+import Foundation
+
+struct Message {
+    
+    let sender: String
+    let body: String
+    
+}
